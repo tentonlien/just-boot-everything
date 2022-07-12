4000bf15252adc59f7e24fe3b722d669e7ca98e9
Tenton Lien <tentonlien@users.noreply.github.com>
2022-07-12 20:56:38 +0800

    Add just-boot-prometheus

---
+++ just-boot-prometheus/src/main/java/com/example/justbootprometheus/JustBootPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.example.justbootprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBootPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustBootPrometheusApplication.class, args);
+	}
+
+}
