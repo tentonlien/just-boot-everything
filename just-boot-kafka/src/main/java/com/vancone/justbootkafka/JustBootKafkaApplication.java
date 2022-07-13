ef3e254ad76b1ec36e3bda214d2df3398af5471a
Tenton Lien <tentonlien@users.noreply.github.com>
2022-07-14 00:48:14 +0800

    Add just-boot-kafka

---
+++ just-boot-kafka/src/main/java/com/vancone/justbootkafka/JustBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.vancone.justbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustBootKafkaApplication.class, args);
+    }
+
+}
---
+++ just-boot-kafka/src/test/java/com/vancone/justbootkafka/JustBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vancone.justbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustBootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
